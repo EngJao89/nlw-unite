d56360729f9d49ff90c8ec96c68c00a4addad8de
EngJao89 <eng_joao@outlook.com>
2024-04-04 09:45:31 -0300

    feat(nlw05-arquitetura-backend): implementa arquitetura do servidor node

---
+++ backend/prisma/migrations/20240404124117_create_table_events/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "events" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "details" TEXT,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "events_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "events_slug_key" ON "events"("slug");
