256d86e0f033b3c9bf6e092ae7c3b8285086b856
EngJao89 <eng_joao@outlook.com>
2024-04-04 10:07:07 -0300

    feat(nlw07-get-event-attendee): implementa busca de partipação em evento

---
+++ backend/prisma/migrations/20240404125736_create_table_attendee/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE "events" ADD COLUMN     "maximum_attendees" INTEGER;
+
+-- CreateTable
+CREATE TABLE "attendees" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "event_id" TEXT NOT NULL,
+
+    CONSTRAINT "attendees_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees"("event_id", "email");
+
+-- AddForeignKey
+ALTER TABLE "attendees" ADD CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE CASCADE ON UPDATE CASCADE;
